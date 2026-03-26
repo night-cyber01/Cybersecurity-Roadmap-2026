8ee6d75db9a3d8e6ba2b1761d0fddf77920efc34
Night Cyber <weavernight29@gmail.com>
2026-03-26 18:49:19 -0400

    feat: complete custom caesar cipher logic

--- caesar_v2.py
+++ caesar_v2.py
@@ -1,4 +1,14 @@
 mensaje = input("Escribe el mensaje:  ")
 resultado = ""
+clave = int(input("Dime la clave:  "))
 for letra in mensaje:
-    print(letra) 
+    if letra.isupper():  
+        numero_ascii = ord(letra)
+        indice = numero_ascii - 65
+        nuevo_indice = (indice + clave) % 26
+        nuevo_ascii = nuevo_indice + 65
+        nueva_letra = chr(nuevo_ascii)
+        resultado = resultado + nueva_letra 
+    else:
+        resultado = resultado + letra
+print(f"Mensaje cifrado: {resultado}")
